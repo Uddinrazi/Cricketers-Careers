47c15cb7349039b16340391c653dccc77ec0bdb8
Uddinrazi <uddinrazi88@gmail.com>
2023-10-03 15:46:23 +0530

    cricketers details page

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const sequelize = require('./util/db')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 
-const app = express();
 
+app.use(express.json());
+app.use(cors())
 app.use(express.static(path.join(__dirname, "/public")))
 
 
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "/public")))
 
 sequelize.sync({force: false})
 .then((resulst) => {
-    appendFile.listen(4000)
+    app.listen(4000)
 }).catch(err => {
     console.log(err)
 })
